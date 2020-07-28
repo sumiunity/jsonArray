23c0aafc29e6fc78454d1fa31b0e7f8420c83372
Niks Home computer <nsumikawa@gmail.com>
2020-07-28 16:24:21 -0400

    added strftime, strptime and timedelta to jsonObject

--- src/jsonObject.js
+++ src/jsonObject.js
@@ -9,19 +9,72 @@
  * :Date: April 29, 2020
  */
 
+
+import moment from 'moment';
+
 import jsonArray from './jsonArray'
 const debug = false
 
 
-export default class jsonObject {
+export default class jsonObject extends Object {
 
-  constructor(json_object) {
+  constructor(data) {
+    super()
 
-    this.json_object = json_object
+
+    const keys = Object.keys(data)
+
+    // copy over the content from the input data
+    for( var index in keys ){
+      const key = keys[index]
+      this[key] = data[key]
+    }
+
   }
 
   // TODO: search for all columns in the Array
-  get columns(){ return Object.keys(this.json_object) }
+  get columns(){ return Object.keys(this) }
+  get keys(){ return Object.keys(this) }
+
+  // converts the specified key to a datetime attribute
+  strptime( key ){
+
+    if( !(this[key] instanceof moment) ){
+      this[key] = moment(this[key])
+    }
+    return this
+  }
+
+  // converts the datetime attribute to a the specified
+  // date format. when the attribute is not a datetime
+  // format, it is first convert before converting to
+  // a string
+  strftime( key, format='YYYY-MM-DD' ){
+
+    // convert to moment datetime if not already done so
+    this.strptime( key )
+
+    this[key] = this[key].format(format)
+
+    return this
+  }
+
+  // adds time to the date attribute. the key is converted
+  // to moment type if it is not already
+  timedelta( key, params={}){
+
+    // convert to moment datetime if not already done so
+    this.strptime( key )
+
+    const param_keys = Object.keys( params )
+    for( var i=0; i < param_keys.length; i++ ){
+      const pkey = param_keys[i]
+      this[key] = this[key].add(params[pkey], pkey)
+
+    }
+
+    return this
+  }
 
   // convert the json Object to a two dimensional json array
   to_jsonArray( key_name, value_name ){
@@ -29,7 +82,6 @@ export default class jsonObject {
     var array = []
     const keys = this.columns
 
-    console.log('i got here...', key_name, value_name)
     // set defaults for the key and value attributes
     if( key_name === undefined ) key_name = 'key'
     if( value_name === undefined ) value_name = 'value'
