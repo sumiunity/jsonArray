8b300b6f0be97f71ea9c92ad6929899075488777
Niks Home computer <nsumikawa@gmail.com>
2020-08-25 14:42:13 -0400

    extended table view to include images and sortable rows

--- src/views/Table.jsx
+++ src/views/Table.jsx
@@ -7,16 +7,18 @@ import {data} from 'jsonArray/tests/data'
 export default function Table( props ){
 
 
-  // var json_table = new jsonArrayTable( data )
-  // var header = json_table.semanticUI()
+  var json_array = new jsonArray( data )
 
-  const table = 'table'
+  json_array = json_array.astype({image: 'image'})
+  console.log( json_array.dtypes)
+  const table = json_array.react.semanticUI.table
+  table.parameters.sortable = true
   // console.log( header)
   return (
     <div>
       <h2>What is this??</h2>
 
-      {table}
+      {table.render()}
     </div>
   )
 
